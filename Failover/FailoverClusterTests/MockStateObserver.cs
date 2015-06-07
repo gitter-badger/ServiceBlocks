66c792f55c21dbb52af405b5ee0fd51ca414a35d
Alex Michel <amishel@gmail.com>
2015-06-07 18:05:31 +0300

    FFailover Cluster SM UT Coverage

---
+++ Failover/FailoverClusterTests/MockStateObserver.cs
@@ -0,0 +1,36 @@
+using System;
+using Automatonymous;
+using ServiceBlocks.Failover.FailoverCluster.StateMachine;
+
+namespace ServiceBlocks.Failover.FailoverClusterTests
+{
+    public class MockStateObserver : IObserver<StateChanged<InstanceState>>
+    {
+        private readonly Action<StateChanged<InstanceState>> _onNextAction;
+        private readonly Action<Exception> _onError;
+        private readonly Action _onCompletedAction;
+
+
+        public MockStateObserver(Action<StateChanged<InstanceState>> onNextAction, Action<Exception> onError = null, Action onCompletedAction = null)
+        {
+            _onNextAction = onNextAction;
+            _onError = onError ?? (ex => { });
+            _onCompletedAction = onCompletedAction ?? (() => { });
+        }
+
+        public void OnNext(StateChanged<InstanceState> value)
+        {
+            _onNextAction(value);
+        }
+
+        public void OnError(Exception error)
+        {
+            _onError(error);
+        }
+
+        public void OnCompleted()
+        {
+            _onCompletedAction();
+        }
+    }
+}
