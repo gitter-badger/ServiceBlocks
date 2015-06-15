193f7f2c1f79c28b83dd6c2f0c21377a5c063d46
Alex Michel <amishel@gmail.com>
2015-06-15 16:49:28 +0300

    Tests

---
+++ Failover/FailoverClusterTests/ClusterMonitorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceBlocks.Failover.FailoverClusterTests
+{
+    [TestClass]
+    public class ClusterMonitorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
